376428a5f366a1cd5d4b9145352b3b5d39cecc4a
8514 <79288367+8514@users.noreply.github.com>
2021-06-18 18:26:58 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var canvas=new fabric.Canvas('myCanvas');
+ball_y=0;
+ball_x=0;
+hole_y=400;
+hole_x=800;
+function load_img(){
+fabric.image.fromURL("golf-h1.png", function(img)
+hole_obj=Img
+hole_obj=scaletowidth(50);  
+hole_obj=scaletoheight(50);
+hole_obj.set(50)
+top:hole_y;
+left:hole_x;
+});
+canvas.add(hole_obj);
+});
+new_image();
+}
